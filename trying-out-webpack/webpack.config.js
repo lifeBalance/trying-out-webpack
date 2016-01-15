0220288e679424aaa30b6422e2eb15d318ae5004
Javier Rodriguez <javierorodriguez@gmail.com>
2016-01-16 00:04:14 +0300

    Add 'Organizing our project' section

--- trying-out-webpack/webpack.config.js
+++ trying-out-webpack/webpack.config.js
@@ -1,6 +1,14 @@
+var path = require("path");
+
 module.exports = {
-  entry: './entry.js',
+  context: path.resolve('app'),
+  entry: './entry',
   output: {
+    path: path.resolve('build/js'),
+    publicPath: '/public/assets/js/',
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: 'public'
   }
 };
